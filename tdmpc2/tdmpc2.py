60ac432d6faa3760e97d2475300698670a5022ed
ibisbill <ziyuhuan@outlook.com>
2024-04-07 16:39:24 -0400

    filter

--- tdmpc2/tdmpc2.py
+++ tdmpc2/tdmpc2.py
@@ -14,7 +14,7 @@ class TDMPC2:
 	and supports both state and pixel observations.
 	"""
 
-	def __init__(self, cfg):
+	def __init__(self, cfg):   
 		self.cfg = cfg
 		self.device = torch.device('cuda')
 		self.model = WorldModel(cfg).to(self.device)
@@ -91,7 +91,7 @@ class TDMPC2:
 			task = torch.tensor([task], device=self.device)
 		z = self.model.encode(obs, task)
 		if self.cfg.mpc:
-			a = self.plan(z, t0=t0, eval_mode=eval_mode, task=task)
+			a = self.plan_reversed(z, t0=t0, eval_mode=eval_mode, task=task)
 		else:
 			a = self.model.pi(z, task)[int(not eval_mode)][0]
 		return a.cpu()
@@ -220,6 +220,85 @@ class TDMPC2:
 		discount = self.discount[task].unsqueeze(-1) if self.cfg.multitask else self.discount
 		return reward + discount * self.model.Q(next_z, pi, task, return_type='min', target=True)
 
+	@torch.no_grad()
+	def plan_reversed(self, z, t0=False, eval_mode=False, task=None):
+		"""
+		Plan a sequence of actions using the learned world model.
+		
+		Args:
+			z (torch.Tensor): Latent state from which to plan.
+			t0 (bool): Whether this is the first observation in the episode.
+			eval_mode (bool): Whether to use the mean of the action distribution.
+			task (Torch.Tensor): Task index (only used for multi-task experiments).
+
+		Returns:
+			torch.Tensor: Action to take in the environment.
+		"""		
+		# Sample policy trajectories
+		action_threshold = 0.5 
+		if self.cfg.num_pi_trajs > 0:
+			pi_actions = torch.empty(self.cfg.horizon, self.cfg.num_pi_trajs, self.cfg.action_dim, device=self.device)
+			_z = z.repeat(self.cfg.num_pi_trajs, 1)
+			for t in range(self.cfg.horizon-1):
+				pi_actions[t] = self.model.pi(_z, task)[1]
+				_z = self.model.next(_z, pi_actions[t], task)
+			pi_actions[-1] = self.model.pi(_z, task)[1]
+
+		# Initialize state and parameters
+		z = z.repeat(self.cfg.num_samples, 1)
+		mean = torch.zeros(self.cfg.horizon, self.cfg.action_dim, device=self.device)
+		std = self.cfg.max_std*torch.ones(self.cfg.horizon, self.cfg.action_dim, device=self.device)
+		if not t0:
+			mean[:-1] = self._prev_mean[1:]
+		actions = torch.empty(self.cfg.horizon, self.cfg.num_samples, self.cfg.action_dim, device=self.device)
+		if self.cfg.num_pi_trajs > 0:
+			actions[:, :self.cfg.num_pi_trajs] = pi_actions
+	
+		# Iterate MPPI
+		for _ in range(self.cfg.iterations):
+
+			# Sample actions
+			sampled_actions = (mean.unsqueeze(1) + std.unsqueeze(1) * torch.randn(self.cfg.horizon, self.cfg.num_samples-self.cfg.num_pi_trajs, self.cfg.action_dim, device=std.device)).clamp(-1, 1)
+			if self.cfg.multitask:
+				sampled_actions = sampled_actions * self.model._action_masks[task]
+			# actions[:, self.cfg.num_pi_trajs:] = (mean.unsqueeze(1) + std.unsqueeze(1) * \
+			# 	torch.randn(self.cfg.horizon, self.cfg.num_samples-self.cfg.num_pi_trajs, self.cfg.action_dim, device=std.device)) \
+			# 	.clamp(-1, 1)
+			# if self.cfg.multitask:
+			# 	actions = actions * self.model._action_masks[task]
+				
+			# filtering actions
+			# 动作最大值小于threshold
+			valid_actions_mask = sampled_actions.abs().max(dim=2)[0] < action_threshold
+			valid_actions_mask = valid_actions_mask.unsqueeze(2).repeat(1, 1, self.cfg.action_dim)
+			actions[:, self.cfg.num_pi_trajs:][~valid_actions_mask] = 0
+
+			# Compute elite actions
+			value = self._estimate_value(z, actions, task).nan_to_num_(0)
+			elite_idxs = torch.topk(value.squeeze(1), self.cfg.num_elites, dim=0).indices
+			elite_value, elite_actions = value[elite_idxs], actions[:, elite_idxs]
+
+			# Update parameters
+			max_value = elite_value.max(0)[0]
+			score = torch.exp(self.cfg.temperature*(elite_value - max_value))
+			score /= score.sum(0)
+			mean = torch.sum(score.unsqueeze(0) * elite_actions, dim=1) / (score.sum(0) + 1e-9)
+			std = torch.sqrt(torch.sum(score.unsqueeze(0) * (elite_actions - mean.unsqueeze(1)) ** 2, dim=1) / (score.sum(0) + 1e-9)) \
+				.clamp_(self.cfg.min_std, self.cfg.max_std)
+			if self.cfg.multitask:
+				mean = mean * self.model._action_masks[task]
+				std = std * self.model._action_masks[task]
+
+		# Select action
+		score = score.squeeze(1).cpu().numpy()
+		actions = elite_actions[:, np.random.choice(np.arange(score.shape[0]), p=score)]
+		self._prev_mean = mean
+		a, std = actions[0], std[0]
+		if not eval_mode:
+			a += std * torch.randn(self.cfg.action_dim, device=std.device)
+		return a.clamp_(-1, 1)
+
+
 	def update_inverse_dynamics(self, obs, action, task):
 		with torch.no_grad():
 			zs = self.model.encode(obs, task)
@@ -243,11 +322,6 @@ class TDMPC2:
 		torch.nn.utils.clip_grad_norm_(self.model._inv_dynamics.parameters(), self.cfg.grad_clip_norm)
 		self.inv_optim.step()
 
-
-			
-  
-		
-		
 
 	def update(self, buffer):
 		"""
